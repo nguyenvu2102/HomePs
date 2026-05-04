cccd02487e48c618f246cf439b68472374edc5c2
Nguyên Vũ Đình <96331406+nguyenvu2102@users.noreply.github.com>
2026-05-04 23:19:18 +0700

    docker update

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+
+FROM maven:3.9.6-eclipse-temurin-17 AS build
+WORKDIR /app
+COPY . .
+RUN mvn clean package -DskipTests
+
+
+FROM tomcat:10.1-jdk17-temurin
+WORKDIR /usr/local/tomcat/webapps/
+
+COPY --from=build /app/target/HomePS.war ./ROOT.war
+
+EXPOSE 8080
+CMD ["catalina.sh", "run"]
--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = System.getenv().getOrDefault("HOMEPS_DB_URL", "jdbc:postgresql://localhost:5432/HomePS");
+    private static final String URL = System.getenv().getOrDefault("HOMEPS_DB_URL", "jdbc:postgresql://db:5432/HomePS");
     private static final String USER = System.getenv().getOrDefault("HOMEPS_DB_USER", "postgres");
     private static final String PASS = System.getenv().getOrDefault("HOMEPS_DB_PASS", "postgres");
 
